af458b5f65cc5b56b6b89503884c13de9781a5eb
Tomer Inbar <tomerinbar1@gmail.com>
2023-04-29 22:31:13 +0300

    Final Project - Meme Generator

--- js/main.js
+++ js/main.js
@@ -7,194 +7,194 @@ let isFlex = false
 const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
 
 function onInit() {
-    gElCanvas = document.querySelector('.canvas')
-    gCtx = gElCanvas.getContext('2d')
-    renderGallery()
-    addListeners()
-    renderCanvas()
-    resizeCanvas(1)
+  gElCanvas = document.querySelector('.canvas')
+  gCtx = gElCanvas.getContext('2d')
+  renderGallery()
+  addListeners()
+  renderCanvas()
+  resizeCanvas(1)
 }
 
 function addListeners() {
-    addMouseListeners()
-    addTouchListeners()
-    window.addEventListener('resize', () => {
-        resizeCanvas(1)
-        renderCanvas()
-    })
+  addMouseListeners()
+  addTouchListeners()
+  window.addEventListener('resize', () => {
+    resizeCanvas(1)
+    renderCanvas()
+  })
 }
 
 function addMouseListeners() {
-    gElCanvas.addEventListener('mousedown', onDown)
-    gElCanvas.addEventListener('mousemove', onMove)
-    gElCanvas.addEventListener('mouseup', onUp)
+  gElCanvas.addEventListener('mousedown', onDown)
+  gElCanvas.addEventListener('mousemove', onMove)
+  gElCanvas.addEventListener('mouseup', onUp)
 }
 
 function addTouchListeners() {
-    gElCanvas.addEventListener('touchstart', onDown)
-    gElCanvas.addEventListener('touchmove', onMove)
-    gElCanvas.addEventListener('touchend', onUp)
+  gElCanvas.addEventListener('touchstart', onDown)
+  gElCanvas.addEventListener('touchmove', onMove)
+  gElCanvas.addEventListener('touchend', onUp)
 }
 
 function toggleIcon(elNav) {
-    elNav.classList.toggle('open')
-    const elNavbar = document.querySelector('.navbar')
-    elNavbar.classList.toggle('open-navbar')
+  elNav.classList.toggle('open')
+  const elNavbar = document.querySelector('.navbar')
+  elNavbar.classList.toggle('open-navbar')
 }
 
 function onOpenEditor() {
-    const elGallery = document.querySelector('.gallery-page-wrapper')
-    const elEditor = document.querySelector('.editor-page-wrapper')
-    const elSaved = document.querySelector('.saved-page-wrapper')
-    const elAbout = document.querySelector('.about-page-wrapper')
-    elAbout.style.display = 'none'
-    elSaved.style.display = 'none'
-    elGallery.style.display = 'none'
-    elEditor.style.display = 'block'
-  }
+  const elGallery = document.querySelector('.gallery-page-wrapper')
+  const elEditor = document.querySelector('.editor-page-wrapper')
+  const elSaved = document.querySelector('.saved-page-wrapper')
+  const elAbout = document.querySelector('.about-page-wrapper')
+  elAbout.style.display = 'none'
+  elSaved.style.display = 'none'
+  elGallery.style.display = 'none'
+  elEditor.style.display = 'block'
+}
 
-  function onOpenGallery() {
-    const elGallery = document.querySelector('.gallery-page-wrapper')
-    const elEditor = document.querySelector('.editor-page-wrapper')
-    const elAbout = document.querySelector('.about-page-wrapper')
-    const elSaved = document.querySelector('.saved-page-wrapper')
-    elAbout.style.display = 'none'
-    elEditor.style.display = 'none'
-    elSaved.style.display = 'none'
-    elGallery.style.display = 'block'
-  }
+function onOpenGallery() {
+  const elGallery = document.querySelector('.gallery-page-wrapper')
+  const elEditor = document.querySelector('.editor-page-wrapper')
+  const elAbout = document.querySelector('.about-page-wrapper')
+  const elSaved = document.querySelector('.saved-page-wrapper')
+  elAbout.style.display = 'none'
+  elEditor.style.display = 'none'
+  elSaved.style.display = 'none'
+  elGallery.style.display = 'block'
+}
 
-  function onOpenSaved() {
-    const elGallery = document.querySelector('.gallery-page-wrapper')
-    const elEditor = document.querySelector('.editor-page-wrapper')
-    const elSaved = document.querySelector('.saved-page-wrapper')
-    const elAbout = document.querySelector('.about-page-wrapper')
-    elEditor.style.display = 'none'
-    elGallery.style.display = 'none'
-    elAbout.style.display = 'none'
-    elSaved.style.display = 'block'
-    renderSavedMemes()
-  }
+function onOpenSaved() {
+  const elGallery = document.querySelector('.gallery-page-wrapper')
+  const elEditor = document.querySelector('.editor-page-wrapper')
+  const elSaved = document.querySelector('.saved-page-wrapper')
+  const elAbout = document.querySelector('.about-page-wrapper')
+  elEditor.style.display = 'none'
+  elGallery.style.display = 'none'
+  elAbout.style.display = 'none'
+  elSaved.style.display = 'block'
+  renderSavedMemes()
+}
 
-  function onOpenAbout() {
-    const elAbout = document.querySelector('.about-page-wrapper')
-    const elGallery = document.querySelector('.gallery-page-wrapper')
-    const elEditor = document.querySelector('.editor-page-wrapper')
-    const elSaved = document.querySelector('.saved-page-wrapper')
-    elEditor.style.display = 'none'
-    elGallery.style.display = 'none'
-    elSaved.style.display = 'none'
-    elAbout.style.display = 'block'
-  }
+function onOpenAbout() {
+  const elAbout = document.querySelector('.about-page-wrapper')
+  const elGallery = document.querySelector('.gallery-page-wrapper')
+  const elEditor = document.querySelector('.editor-page-wrapper')
+  const elSaved = document.querySelector('.saved-page-wrapper')
+  elEditor.style.display = 'none'
+  elGallery.style.display = 'none'
+  elSaved.style.display = 'none'
+  elAbout.style.display = 'block'
+}
 
 function resizeCanvas(ratio) {
-    const elContainer = document.querySelector('.canvas-container')
-    gElCanvas.width = elContainer.offsetWidth
-    gElCanvas.height = elContainer.offsetHeight * ratio
+  const elContainer = document.querySelector('.canvas-container')
+  gElCanvas.width = elContainer.offsetWidth
+  gElCanvas.height = elContainer.offsetHeight * ratio
 }
 
 function renderCanvas() {
-    const meme = getMeme()
-    const img = new Image()
-    img.src = `./img/${meme.selectedImgId}.jpg`
-    img.onload = () => {
-        const ratio = img.naturalHeight / img.naturalWidth
-        resizeCanvas(ratio)
-        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-        drawText()
-        markLine()
-    }
+  const meme = getMeme()
+  const img = new Image()
+  img.src = `./img/${meme.selectedImgId}.jpg`
+  img.onload = () => {
+    const ratio = img.naturalHeight / img.naturalWidth
+    resizeCanvas(ratio)
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    drawText()
+    markLine()
+  }
 }
 
 function renderGallery() {
-    const imgs = getImgs()
-    const elGallery = document.querySelector('.gallery')
-    const imgsStr = imgs.map(img => {
-        return `<li onclick="onSetImg(${img.id})"><img class="gallery-img" src="${img.url}" /></li>`
-    })
-    imgsStr.unshift('<ul class="gallery-grid">')
-    imgsStr.push('</ul>')
-    elGallery.innerHTML = imgsStr.join('')
+  const imgs = getImgs()
+  const elGallery = document.querySelector('.gallery')
+  const imgsStr = imgs.map(img => {
+    return `<li onclick="onSetImg(${img.id})"><img class="gallery-img" src="${img.url}" /></li>`
+  })
+  imgsStr.unshift('<ul class="gallery-grid">')
+  imgsStr.push('</ul>')
+  elGallery.innerHTML = imgsStr.join('')
 
-    const elImgs = document.querySelectorAll('.gallery-img')
+  const elImgs = document.querySelectorAll('.gallery-img')
 
-    elImgs.forEach(elImg => {
-        const imgNaturalWidth = elImg.naturalWidth
-        const imgNaturalHeight = elImg.naturalHeight
-        const imgRatio = imgNaturalHeight / imgNaturalWidth
-        elImg.height = 150 * imgRatio
-        elImg.width = 150
-    })
+  elImgs.forEach(elImg => {
+    const imgNaturalWidth = elImg.naturalWidth
+    const imgNaturalHeight = elImg.naturalHeight
+    const imgRatio = imgNaturalHeight / imgNaturalWidth
+    elImg.height = 150 * imgRatio
+    elImg.width = 150
+  })
 }
 
 function drawText() {
-    const meme = getMeme()
+  const meme = getMeme()
 
-    meme.lines.forEach(line => {
-        gCtx.beginPath()
-        gCtx.font = `${line.size}px ${line.font}`
-        const textWidth = gCtx.measureText(line.txt).width
-        line.textWidth = textWidth
-        gCtx.textBaseline = 'alphabetic'
+  meme.lines.forEach(line => {
+    gCtx.beginPath()
+    gCtx.font = `${line.size}px ${line.font}`
+    const textWidth = gCtx.measureText(line.txt).width
+    line.textWidth = textWidth
+    gCtx.textBaseline = 'alphabetic'
 
-        gCtx.fillStyle = line.color
-        gCtx.strokeStyle = `${line.strokeStyle}`
-        gCtx.fillStyle = `${line.color}`
-        gCtx.lineWidth = 1
-        gCtx.textAlign = `${line.align}`
-        gCtx.fillText(line.txt, line.pos.x, line.pos.y)
-        gCtx.strokeText(line.txt, line.pos.x, line.pos.y)
-    })
+    gCtx.fillStyle = line.color
+    gCtx.strokeStyle = `${line.strokeStyle}`
+    gCtx.fillStyle = `${line.color}`
+    gCtx.lineWidth = 1
+    gCtx.textAlign = `${line.align}`
+    gCtx.fillText(line.txt, line.pos.x, line.pos.y)
+    gCtx.strokeText(line.txt, line.pos.x, line.pos.y)
+  })
 }
 
 function markLine() {
-    const meme = getMeme()
-    meme.lines.map((line, idx) => {
-        if (idx === meme.selectedLineIdx) {
-            gCtx.beginPath()
-            var rectY = line.pos.y - line.size
-            var rectX = line.pos.x - line.textWidth / 2
-            var rectWidth = line.textWidth
-            var rectHeight = 1.5 * line.size
-            gCtx.lineWidth = 3
-            gCtx.rect(rectX - 10, rectY, rectWidth + 20, rectHeight)
-            gCtx.strokeStyle = 'pink'
-            gCtx.stroke()
-        }
-    })
+  const meme = getMeme()
+  meme.lines.map((line, idx) => {
+    if (idx === meme.selectedLineIdx) {
+      gCtx.beginPath()
+      var rectY = line.pos.y - line.size
+      var rectX = line.pos.x - line.textWidth / 2
+      var rectWidth = line.textWidth
+      var rectHeight = 1.5 * line.size
+      gCtx.lineWidth = 3
+      gCtx.rect(rectX - 10, rectY, rectWidth + 20, rectHeight)
+      gCtx.strokeStyle = 'pink'
+      gCtx.stroke()
+    }
+  })
 }
 
 function renderSavedMemes() {
-    const savedMemes = getSavedMemes()
-    const elSavedContainer = document.querySelector('.saved-container')
-    const imgsStr = savedMemes.map(img => {
-        return `<li onclick="onOpenSavedMeme('${img.id}')"><img class="gallery-img" src="${img.saveBase}" /></li>`
-    })
-    imgsStr.unshift('<ul class="gallery-saved-grid">')
-    imgsStr.push('</ul>')
-    elSavedContainer.innerHTML = imgsStr.join('')
+  const savedMemes = getSavedMemes()
+  const elSavedContainer = document.querySelector('.saved-container')
+  const imgsStr = savedMemes.map(img => {
+    return `<li onclick="onOpenSavedMeme('${img.id}')"><img class="gallery-img" src="${img.saveBase}" /></li>`
+  })
+  imgsStr.unshift('<ul class="gallery-saved-grid">')
+  imgsStr.push('</ul>')
+  elSavedContainer.innerHTML = imgsStr.join('')
 }
 
 function onFlexible() {
-    isFlex = true
-    const meme = getMeme()
-    const lines = meme.lines
-    lines.splice(0, lines.length)
-    const imgs = getImgs()
-    const randImgId = getRandomIntInclusive(0, imgs.length - 1)
-    onSetImg(randImgId)
-    const lineCount = getRandomIntInclusive(1, 2)
-    var randLines = []
-    for (let i = 0; i < lineCount; i++) {
-        const randLine = createLine(
-            getRandomText(),
-            getRandomIntInclusive(30, 40),
-            getRandomColor(),
-            getRandomColor(),
-            {x: 250, y: i === 0 ? 100 : 250}
-        )
-        randLines.push(randLine)
-    }
-    meme.lines = randLines
-    renderCanvas()
+  isFlex = true
+  const meme = getMeme()
+  const lines = meme.lines
+  lines.splice(0, lines.length)
+  const imgs = getImgs()
+  const randImgId = getRandomIntInclusive(0, imgs.length - 1)
+  onSetImg(randImgId)
+  const lineCount = getRandomIntInclusive(1, 2)
+  var randLines = []
+  for (let i = 0; i < lineCount; i++) {
+    const randLine = createLine(
+      getRandomText(),
+      getRandomIntInclusive(30, 40),
+      getRandomColor(),
+      getRandomColor(),
+      { x: 250, y: i === 0 ? 100 : 250 }
+    )
+    randLines.push(randLine)
+  }
+  meme.lines = randLines
+  renderCanvas()
 }
--- js/service/util.js
+++ js/service/util.js
@@ -1,64 +1,64 @@
 'use strict'
 
 function getRandomIntInclusive(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function getRandomColor() {
-    const color = '#' + Math.floor(Math.random() * 16777215).toString(16)
-    return color
+  const color = '#' + Math.floor(Math.random() * 16777215).toString(16)
+  return color
 }
 
 function downloadImg(elLink) {
-    const imgContent = gElCanvas.toDataURL('image/jpeg')
-    elLink.href = imgContent
+  const imgContent = gElCanvas.toDataURL('image/jpeg')
+  elLink.href = imgContent
 }
 
 // share on Facebook
 
 function onUploadImg() {
-    const imgDataUrl = gElCanvas.toDataURL('image/jpeg')
+  const imgDataUrl = gElCanvas.toDataURL('image/jpeg')
 
-    function onSuccess(uploadedImgUrl) {
-        const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
-        console.log(encodedUploadedImgUrl)
-        window.open(
-            `https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}`
-        )
-    }
+  function onSuccess(uploadedImgUrl) {
+    const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+    console.log(encodedUploadedImgUrl)
+    window.open(
+      `https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}`
+    )
+  }
 
-    doUploadImg(imgDataUrl, onSuccess)
+  doUploadImg(imgDataUrl, onSuccess)
 }
 
 function doUploadImg(imgDataUrl, onSuccess) {
-    const formData = new FormData()
-    formData.append('img', imgDataUrl)
-    const XHR = new XMLHttpRequest()
-    XHR.onreadystatechange = () => {
-        if (XHR.readyState !== XMLHttpRequest.DONE) return
-        if (XHR.status !== 200) return console.error('Error uploading image')
-        const {responseText: url} = XHR
-        console.log('Got back live url:', url)
-        onSuccess(url)
-    }
-    XHR.onerror = (req, ev) => {
-        console.error(
-            'Error connecting to server with request:',
-            req,
-            '\nGot response data:',
-            ev
-        )
-    }
-    XHR.open('POST', '//ca-upload.com/here/upload.php')
-    XHR.send(formData)
+  const formData = new FormData()
+  formData.append('img', imgDataUrl)
+  const XHR = new XMLHttpRequest()
+  XHR.onreadystatechange = () => {
+    if (XHR.readyState !== XMLHttpRequest.DONE) return
+    if (XHR.status !== 200) return console.error('Error uploading image')
+    const { responseText: url } = XHR
+    console.log('Got back live url:', url)
+    onSuccess(url)
+  }
+  XHR.onerror = (req, ev) => {
+    console.error(
+      'Error connecting to server with request:',
+      req,
+      '\nGot response data:',
+      ev
+    )
+  }
+  XHR.open('POST', '//ca-upload.com/here/upload.php')
+  XHR.send(formData)
 }
 
 function makeId(length = 5) {
-    let txt = ''
-    const possible =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for (var i = 0; i < length; i++) {
-        txt += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-    return txt
+  let txt = ''
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return txt
 }
